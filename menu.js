3312a874a4000bc5e294356f5390d8dd50a59a0b
Egyedi Róbert <egyedirobika@gmail.com>
2023-06-02 10:14:51 +0200

    Mappákba rendszerezés és gomb design

--- menu.js
+++
@@ -1,6 +0,0 @@
-const menuToggle = document.getElementById('menu-toggle');
-const sideMenu = document.getElementById('side-menu');
-
-menuToggle.addEventListener('click', () => {
-  sideMenu.classList.toggle('show');
-});
