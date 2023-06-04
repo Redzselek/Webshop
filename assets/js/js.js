a44505d78ace66d683275eb44f29cd8c286b752a
Egyedi Róbert <egyedirobika@gmail.com>
2023-06-04 13:41:52 +0200

    Hozzálehet adni a kosárhoz a dolgokat
    A CSS-t értem, a JS-t részenként nem

--- assets/js/js.js
+++ assets/js/js.js
@@ -71,6 +71,7 @@ let products = [
   
 ];
 
+let listCards  = [];
 function initApp(){
   products.forEach((value, key) =>{
       let newDiv = document.createElement('div');
@@ -84,3 +85,38 @@ function initApp(){
   })
 }
 initApp();
+
+function addToCard(key){
+  if(listCards[key] == null){
+      // Termék másolása a listából a kártyákba
+      listCards[key] = JSON.parse(JSON.stringify(products[key]));
+      listCards[key].quantity = 1;
+      //Igazából nem értem hogy működik ez...
+  }
+  reloadCard();
+}
+
+function reloadCard(){
+  listCard.innerHTML = '';
+  let count = 0;
+  let totalPrice = 0;
+  listCards.forEach((value, key)=>{
+      totalPrice = totalPrice + value.price;
+      count = count + value.quantity;
+      if(value != null){
+          let newDiv = document.createElement('li');
+          newDiv.innerHTML = `
+              <div><img src="assets/images/${value.image}"/></div>
+              <div>${value.name}</div>
+              <div>${value.price.toLocaleString()}</div>
+              <div>
+                  <button onclick="changeQuantity(${key}, ${value.quantity - 1})">-</button>
+                  <div class="db">${value.quantity}</div>
+                  <button onclick="changeQuantity(${key}, ${value.quantity + 1})">+</button>
+              </div>`;
+              listCard.appendChild(newDiv);
+      }
+  })
+  total.innerText = totalPrice.toLocaleString()+"ft";
+  quantity.innerText = count;
+}
